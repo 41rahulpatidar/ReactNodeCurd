8762da1901a44078003745159cb3e59a258235f4
Rahul <you@example.com>
2019-02-12 17:31:05 +0530

    first commit

---
+++ src/components/create.component.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class Create extends Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      person_name: '',
+      business_name: '',
+      business_gst_number:''
+    }
+  }
+  handleChange(event) {
+    console.log(event)
+    const target = event.target;
+    const name = target.name;
+    const value = target.value;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    const obj = {
+      person_name: this.state.person_name,
+      business_name: this.state.business_name,
+      business_gst_number: this.state.business_gst_number
+    };
+    axios.post('http://localhost:4000/business/add', obj)
+        .then(res => console.log(res.data));
+    
+    this.setState({
+      person_name: '',
+      business_name: '',
+      business_gst_number: ''
+    })
+  }
+ 
+  render() {
+    return (
+        <div style={{ marginTop: 10 }}>
+            <h3 align="center">Add New Business</h3>
+            <form onSubmit={this.onSubmit}>
+                <div className="form-group">
+                    <label>Person Name:  </label>
+                    <input 
+                      type="text" 
+                      className="form-control" 
+                      name="person_name"
+                      value={this.state.person_name} onChange={this.handleChange}
+                      />
+                </div>
+                <div className="form-group">
+                    <label>Business Name: </label>
+                    <input type="text" 
+                      className="form-control"
+                      name="business_name"
+                      value={this.state.business_name}
+                      onChange={this.handleChange}
+                      />
+                </div>
+                <div className="form-group">
+                    <label>GST Number: </label>
+                    <input type="text" 
+                      className="form-control"
+                      name="business_gst_number"
+                      value={this.state.business_gst_number}
+                      onChange={this.handleChange}
+                      />
+                </div>
+                <div className="form-group">
+                    <input type="submit" 
+                      value="Register Business" 
+                      className="btn btn-primary"/>
+                </div>
+            </form>
+        </div>
+    )
+  }
+}
